e4c04cee5f9ee6d054304daf592fc166a804daee
SakisKarakasiliotis <dion.k.karak@gmail.com>
2016-07-28 20:36:40 +0300

    fixed the error

--- src/BidwarsMain/Main.java
+++ src/BidwarsMain/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+//import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import model.BidwarsUser;
--- src/db/CategoryDB.java
+++ src/db/CategoryDB.java
@@ -9,7 +9,6 @@ import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 
 import entities.BidwarsCategory;
-import entities.BidwarsProduct;
 
 public class CategoryDB {
 	@SuppressWarnings("unchecked")
